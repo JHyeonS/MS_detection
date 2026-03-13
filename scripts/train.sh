aabb4d51fa760927d6ff25e1a886a6bb74b428c1
Hyeonsu J <ted011204@gmail.com>
2026-03-13 13:17:14 +0900

    real

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,5 +7,5 @@ BASE_CONFIG=config/base.yaml
 CONFIG=config/train.yaml
 
 python src/training/trainer_finetune.py \
-  --base_config $BASE_CONFIG \
-  --config $CONFIG
+  --base_cfg $BASE_CONFIG \
+  --stage_cfg $CONFIG
